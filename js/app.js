b86bc037098a7392cfadf3fcf241b99b070a6e50
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-20 16:16:47 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,70 @@
+console.log("HW");
+
+
+//VERBAL QUESTIONS
+
+//1. What is the difference between a parameter and an argument?
+   //A parameter is a variable listed that is part of the function. (inside)
+   //An argument is a variable that is given to the function when told to. (out)
+
+
+//2. Within a function, what is the difference between return and console.log?
+   //A function stores the data in the memory
+   //console.log only writes it into the console. It's also a cool tool to check work.
+
+
+//3. What are the implications of the ability of a function to return a value?
+	// Return functions cannot be checked in the console and you cannot write code 
+	//after the return function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
